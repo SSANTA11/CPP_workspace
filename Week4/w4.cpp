59d0e2143700f946810f4e0dc3343fab4a3013e6
SSANTA11 <ssanta011205@skuniv.ac.kr>
2025-09-24 12:02:07 +0900

    4주차 CPP 학습본

--- Week4/w4.cpp
+++ Week4/w4.cpp
@@ -58,7 +58,7 @@ public:
 
     Circle(int r) {
         radius = r;
-        cout << "반지름 "<<radius<<"원 소멸\n";
+        cout << "반지름 "<<radius<<"원 생성\n";
     }
 
     ~Circle() {
@@ -73,8 +73,93 @@ public:
         return 3.14 * radius * radius;
     }
 };
+//
+//// 아래 디버깅 시 발생과 소멸 순서 기억하기(코드블록과 스택으로 인한 출력값 예측 중요)
+//Circle gDonut(1000);// 전역(global)==외부변수
+//Circle gPizza(2000);
+//
+//void f() {
+//    Circle fDonut(100);
+//    Circle fPizza(200);
+//}
+//
+//int main() {
+//    Circle donut; // 지역(local)--> 소속 block 내에서 선언
+//    Circle pizza(30); 
+//    f();  
+//}
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+// 생성자를 private으로 제한을 두어 무분별한 객체 생성을 제약을 주는 경우가 존재한다. ---> 왜>>??? 검색하기
+//#include <iostream>
+//using namespace std;
+//
+//class Circle {
+//private:
+//    int radius;
+//
+//    Circle(): Circle(1) {}
+//
+//    Circle(int r) {
+//        radius = r;
+//        cout << "반지름 "<<radius<<"원 생성\n";
+//    }
+//
+//public:
+//    ~Circle() {
+//        cout << "반지름 "<<radius<<"원 소멸\n";
+//    }
+//
+//    void setRadius(int r) {
+//        radius = r;
+//    }
+//
+//    double getArea() {
+//        return 3.14 * radius * radius;
+//    }
+//};
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+// 자바의 기본 타입 변수들 int double flaot char boolean ...
+// C++에서는 기본타입 객체 배열을 참조 기본값 포인트로 가능???하다 이는 자바와 큰차이 ---> 시험##################################################
+
+
+//int main() {
+//    Circle donut;
+//    Circle pizza(30);
+//
+//    cout << donut.getArea()<<endl;
+//
+//    Circle* p;
+//    //p->getArea(); 윗줄에 이어서 포인터의 초기화 없이 이처럼 쓴다면, 문제 발생
+//
+//    p = &donut;
+//    cout << p->getArea()<<endl;
+//    cout << (*p).getArea() << endl;
+//    p = &pizza;
+//    cout << p->getArea()<<endl;
+//    cout << (*p).getArea() << endl;
+//
+//}
+
+// 자바에서는 문제를 언어가 메모리 관리를 하지만, C++은 직접한다.
+
+
+// 정수형 공간의 동적 할당 및 반환
 
 int main() {
-    Circle donut;
-    Circle pizza(30);
+    int* p;
+
+    p = new int;
+    if (!p) {
+        cout << "메모리를 할당할 수 없습니다." << endl;
+        return 0;
+    }
+    *p = 5; //할당받은 정수 공간에 5 대입
+    int n = *p;
+    cout << "*p= "<<*p << endl;
+    cout << "n= "<<n << endl;
+    delete p;
 }
